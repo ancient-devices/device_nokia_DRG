6d2ac5e849c2382e978f04924945bd195e91832b
ANKIT KENE <keneankit01@gmail.com>
2021-07-07 16:13:33 +0530

    add Fluid

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,10 +15,12 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_DRG.mk
-
+    $(LOCAL_DIR)/lineage_DRG.mk \
+    $(LOCAL_DIR)/fluid_DRG.mk
 COMMON_LUNCH_CHOICES := \
     lineage_DRG-eng \
     lineage_DRG-user \
-    lineage_DRG-userdebug
-
+    lineage_DRG-userdebug \
+    fluid_DRG-eng \
+    fluid_DRG-user \
+    fluid_DRG-userdebug
