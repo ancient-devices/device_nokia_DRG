3faabe1994ba9aaab6cf6616d7bcbd447a1302cd
ANKIT KENE <keneankit01@gmail.com>
2021-09-21 14:22:10 +0000

    cleanup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,26 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_DRG.mk \
-    $(LOCAL_DIR)/fluid_DRG.mk \
-    $(LOCAL_DIR)/aosp_DRG.mk \
-    $(LOCAL_DIR)/evolution_DRG.mk \	
-    $(LOCAL_DIR)/ancient_DRG.mk \	
-	
+    $(LOCAL_DIR)/ancient_DRG.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_DRG-eng \
-    lineage_DRG-user \
-    lineage_DRG-userdebug \
-    fluid_DRG-eng \
-    fluid_DRG-user \
-    fluid_DRG-userdebug \
-    aosp_DRG-eng \
-    aosp_DRG-user \
-    aosp_DRG-userdebug \
-    evolution_DRG-eng \
-    evolution_DRG-user \
-    evolution_DRG-userdebug \
     ancient_DRG-eng \
     ancient_DRG-user \
-    ancient_DRG-userdebug 
+    ancient_DRG-userdebug
